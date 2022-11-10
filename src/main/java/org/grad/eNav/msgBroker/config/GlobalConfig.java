b617923a13d02f61f0ba7f4cf43aa795477deadf
Nikolaos Vastardis <n.vastardis@gmail.com>
2022-11-10 17:00:30 +0000

    Enabling the http trace actuator

---
+++ src/main/java/org/grad/eNav/msgBroker/config/GlobalConfig.java
@@ -0,0 +1,38 @@
+package org.grad.eNav.msgBroker.config;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The Global Configuration.
+ *
+ * A class to define the global configuration for the application.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Configuration
+public class GlobalConfig {
+
+    /**
+     * <p>
+     * Add an HTTP trace repository in memory to be used for the respective
+     * actuator.
+     * </p>
+     * <p>
+     * The functionality has been removed by default in Spring Boot 2.2.0. For
+     * more info see:
+     * </p>
+     * <a href="https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.2.0-M3-Release-Notes#actuator-http-trace-and-auditing-are-disabled-by-default">...</a>
+     *
+     * @return the in memory HTTP trance repository
+     */
+    @ConditionalOnProperty(value = "management.trace.http.enabled", havingValue = "true")
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+}
